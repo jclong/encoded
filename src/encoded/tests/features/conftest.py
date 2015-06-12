f332c8874a5119b24b353a6f93db7a1147ac15a2
Laurence Rowe <laurence@lrowe.co.uk>
2015-06-12 15:43:23 -0700

    Disable load condition

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -47,13 +47,12 @@ def base_url(_server):
     return _server
 
 
-@pytest.fixture(scope='session')
-def splinter_browser_load_condition():
-
-    def condition(browser):
-        return browser.is_element_not_present_by_css(".communicating")
-
-    return condition
+# @pytest.fixture(scope='session')
+# def splinter_browser_load_condition():
+#
+#     def condition(browser):
+#         return browser.is_element_not_present_by_css(".communicating")
+#     return condition
 
 
 @pytest.fixture(scope='session')
